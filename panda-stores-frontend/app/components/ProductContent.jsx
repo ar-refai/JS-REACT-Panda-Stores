e62ab84e49bb53232776ecc5f2e241bc4e2ec161
Abdelrahman Elrefai <team.man00@gmail.com>
2024-02-24 18:46:41 +0200

    finished home and productDetails pages

---
+++ panda-stores-frontend/app/components/ProductContent.jsx
@@ -0,0 +1,98 @@
+'use client'
+import React , {useState} from 'react'
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '@/app/components';
+import { urlFor } from '@/lib/client';
+const ProductContent = ({details,image,name,price,products}) => {
+    const [index, setIndex] = useState(0);
+return (
+    <>
+    <div>
+            <div className="product-detail-container">
+                <div className="image-container">
+                    <img 
+                    src={urlFor(image && image[index]).url()} 
+                    className='product-detail-image'
+                    />
+                <div className="small-images-container">
+                    {image?.map((item,i)=> {
+                        return (
+                            <img 
+                            src={urlFor(item).url()} 
+                            alt='product' 
+                            className={i === index ? ' small-image selected-image' : 'small-image'} 
+                            onMouseEnter={()=>{setIndex(i)}}
+                        />
+                        )
+                    })
+                    }
+                </div>
+                </div>
+                <div className="product-detail-desc">
+                    <h1>
+                        {name}
+                    </h1>
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
+                        </div>
+                        <p>
+                            20
+                        </p>
+                    </div>
+                    <h4>Details</h4>
+                    <p>{details}</p>
+                    <p className='price'>${price}</p>
+                    <div className="quantity">
+                        <h3>
+                            Quantity:
+                        </h3>
+                        <p className='quantity-desc'>
+                            <span className="minus" onClick=''>
+                                <AiOutlineMinus />
+                            </span>
+                            <span className="num" onClick=''>
+                                0
+                            </span>
+                            <span className="plus" onClick=''>
+                                <AiOutlinePlus />
+                            </span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button type='button' className='add-to-cart' onCLick=''>
+                            Add to Cart
+                        </button>
+                        <button type='button' className='buy-now' onCLick=''>
+                        buy-now                       
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className='marquee'>
+                    <div className="maylike-products-container track">
+                        {
+                            products?.map((item)=>{
+                                return (
+                                    <Product 
+                                    key={item._id} 
+                                    product = {item}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+)
+}
+
+export default ProductContent
--- panda-stores-frontend/app/product/[slug]/page.js
+++ panda-stores-frontend/app/product/[slug]/page.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { client  , urlFor } from '@/lib/client';
-export async function GetStaticProps({params : slug }) {
-    // function for getting the data from database
-    const query = `*[_type == "product" && slug.current == '${slug}']`;
-    const products = await client.fetch(query);
-    const bannerQuery = '*[_type == "banner"]';
-    const bannerData = await client.fetch(bannerQuery);
-    return { products, bannerData }
-}
-const ProductDetails = () => {
+import { client, urlFor } from '@/lib/client';
+import ProductContent from '@/app/components/ProductContent';
+
+const ProductDetails = async ({ params: { slug } }) => {
+    let index = 0;
+    // **********************************************
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const product = await client.fetch(query);
+    const productsQuery = '*[_type == "product"]';
+    console.log('*'.repeat(40));
+    const products = await client.fetch(productsQuery);
+    const { image, name, details, price } = product;
+    // ********************************************** 
     return (
-        <div>
-            <div className="product-detail-container">
-                <div className="image-container">
-                    <img src='' />
-                </div>
-            </div>
-        </div>
+        <ProductContent 
+        image = {image} 
+        name = {name} 
+        details = {details} 
+        price = {price}
+        products = {products}
+        />
     )
 }
 
-export default ProductDetails
+export default ProductDetails;
