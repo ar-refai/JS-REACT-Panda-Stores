90d751a0499b112d4a35bf1eb2425ea1d0b4d540
Abdelrahman Elrefai <team.man00@gmail.com>
2024-03-02 14:45:46 +0200

    tab icon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Panda Stores</title>
       </Head>
       <header>
         <Navbar />
